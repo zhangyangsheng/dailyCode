b0e01a7185454d3ace36a18516c163946faaf20a
zhangyangsheng <javaer@outlook.com>
2013-05-27 23:43:02 +0800

    modify availableProcessor with free memory

--- src/AvailableProcessor.java
+++ src/AvailableProcessor.java
@@ -8,5 +8,6 @@ public class AvailableProcessor {
 	public static void main(String[] args) {
 		System.out.println("os name: " + System.getProperty("os.name"));
 		System.out.println("available processors: " + Runtime.getRuntime().availableProcessors());
+		System.out.println("free memory" + Runtime.getRuntime().freeMemory());
 	}
 }
