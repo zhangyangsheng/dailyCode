e0cbfd04d04935bce1f6cd1343559efcdabf66d8
Îñ¹Û <wuguan.zys@taobao.com>
2013-07-01 08:41:24 +0800

    垃圾
    delete

---
+++ bin/concurrency-code/defineAkkaActorsJar.sh
@@ -0,0 +1,16 @@
+#---
+# Excerpted from "Programming Concurrency on the JVM",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/vspcon for more book information.
+#---
+export AKKA_JARS="$AKKA_HOME/lib/scala-library.jar:\
+$AKKA_HOME/lib/akka/akka-stm-1.1.3.jar:\
+$AKKA_HOME/lib/akka/akka-actor-1.1.3.jar:\
+$AKKA_HOME/lib/akka/multiverse-alpha-0.6.2.jar:\
+$AKKA_HOME/lib/akka/akka-typed-actor-1.1.3.jar:\
+$AKKA_HOME/lib/akka/aspectwerkz-2.2.3.jar:\
+$AKKA_HOME/config:\
+."
